c34ddfa3d9fa7af589ef01b1ab00d92f3880b0a3
AlvaroPM <alvaropavonmartinez7@gmail.com>
2025-03-25 08:52:09 +0100

    update generador
    añadido la descarga de la imagen

--- Generador.py
+++ Generador.py
@@ -6,7 +6,7 @@ import os
 # Configuración del servidor
 PORT = 8000
 DIRECTORY = r"C:\Users\ÁlvaroPavón\OneDrive - PLANTASUR TRADING SL\Escritorio\PruebaConexion"
-IP_LOCAL = "192.168.1.94"  
+IP_LOCAL = "192.168.1.94"  # Reemplaza esto con tu dirección IP local
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -15,11 +15,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 def generar_galeria():
-    imagenes = [f for f in os.listdir(DIRECTORY) if f.endswith(('.png', '.jpg', '.jpeg', '.gif','jfif'))]
-    html_content = '<html><head><title>Galería de Fotos</title></head><body>'
+    extensiones_imagenes = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp','jfif')
+    imagenes = [f for f in os.listdir(DIRECTORY) if f.lower().endswith(extensiones_imagenes)]
+    html_content = '<html><head><title>Galería de Fotos</title></head>'
+    html_content += '<style>img { width: 200px; height: 200px; object-fit: cover; margin: 10px; }</style>'
+    html_content += '</head><body>'
     html_content += '<h1>Galería de Fotos</h1>'
     for imagen in imagenes:
-        html_content += f'<img src="{imagen}" style="width:200px;height:auto;margin:10px;">'
+        html_content += f'<div><a href="{imagen}" download><img src="{imagen}" alt="{imagen}"></a></div>'
     html_content += '</body></html>'
     
     with open(os.path.join(DIRECTORY, 'index.html'), 'w') as f:
